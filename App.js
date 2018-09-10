28c73ccec335021bb0d2bf4888115e628c332fd3
Nuwan <w1608508@my.westminster.ac.uk>
2018-09-10 15:17:59 +0530

    Integrate redux

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import CounterApp from './src/CounterApp';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'INCREASE_COUNTER':
+      return {
+        counter: state.counter + 1
+      }
+    case 'DECREASE_COUNTER':
+      return {
+        counter: state.counter - 1
+      }
+  }
+  return state;
+}
+
+const store = createStore(reducer);
 
 export default class App extends React.Component {
 
-  state = {
-    counter: 0
-  }
-
-  increaseCounter = () => {
-    this.setState({counter:this.state.counter+1})
-  }
-
-  decreaseCounter = () => {
-    this.setState({counter:this.state.counter-1})
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <View style = {{ flexDirection: 'row', width: 200,
-      justifyContent: 'space-around'}}>
-          <TouchableOpacity onPress = {()=>this.increaseCounter()}>
-            <Text style = {{fontSize:20}}>Increase</Text>
-          </TouchableOpacity>
-          <Text style = {{fontSize:20}}>{this.state.counter}</Text>
-          <TouchableOpacity onPress = {()=>this.decreaseCounter()}>
-            <Text style = {{fontSize:20}}>Decrease</Text>
-          </TouchableOpacity>    
-        </View>
-      </View>
+      <Provider store = {store}>
+        <CounterApp/>
+      </Provider>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -41,3 +44,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/**
+ * Store - Holds our state (THERE IS ONLY 1 STATE)
+ * Action - State stored in the Store can only be modified with actions
+ * Dispatcher - Action needs to be sent to someone (KNOWN AS DISPATCHING AN ACTION)
+ * Reducer - Recieves the action and modifies the state to give us a new state (STATE IS READ ONLY, IT IS MERELY)
+ * Subscriber - Listens to state and updatest the UI
+ */
